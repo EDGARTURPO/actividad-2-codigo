0dd8cfd11b77ed648238f8f76084e5c22a040de9
Franklin Espinoza <franklin.espinozae02@gmail.com>
2026-05-11 19:34:26 -0500

    diseño de carrito

--- script.js
+++ script.js
@@ -1,17 +1,92 @@
 const botonesAgregar = document.querySelectorAll(".btn-agregar");
 
-const contador = document.querySelector(".contador");
+const contadorCarrito = document.getElementById("contador-carrito");
 
-let cantidad = 0;
+const carritoIcono = document.querySelector(".carrito");
+
+const panelCarrito = document.getElementById("panel-carrito");
+
+const cerrarCarrito = document.getElementById("cerrar-carrito");
+
+const carritoProductos = document.getElementById("carrito-productos");
+
+const totalCarrito = document.getElementById("total-carrito");
+
+let carrito = [];
 
 botonesAgregar.forEach((boton) => {
   boton.addEventListener("click", () => {
-    cantidad++;
+    const producto = boton.closest(".producto-card");
 
-    contador.textContent = cantidad;
+    const nombre = producto.querySelector("h4").textContent;
+
+    const precio = producto.querySelector(".precio").textContent;
+
+    const productoInfo = {
+      nombre,
+      precio,
+    };
+
+    carrito.push(productoInfo);
+
+    actualizarCarrito();
   });
 });
 
+// ABRIR CARRITO
+
+carritoIcono.addEventListener("click", () => {
+  panelCarrito.classList.add("activo");
+});
+
+// CERRAR CARRITO
+
+cerrarCarrito.addEventListener("click", () => {
+  panelCarrito.classList.remove("activo");
+});
+
+function actualizarCarrito() {
+  // ACTUALIZAR CONTADOR
+  contadorCarrito.textContent = carrito.length;
+
+  // LIMPIAR PANEL
+  carritoProductos.innerHTML = "";
+
+  // TOTAL
+  let total = 0;
+
+  carrito.forEach((producto, index) => {
+    const item = document.createElement("div");
+
+    item.classList.add("item-carrito");
+
+    item.innerHTML = `
+    <div class="item-info">
+        <h4>${producto.nombre}</h4>
+        <p>${producto.precio}</p>
+    </div>
+
+    <button class="eliminar-item">
+        ✕
+    </button>
+`;
+
+    carritoProductos.appendChild(item);
+    const botonEliminar = item.querySelector(".eliminar-item");
+
+    botonEliminar.addEventListener("click", () => {
+      carrito.splice(index, 1);
+
+      actualizarCarrito();
+    });
+
+    // SUMAR TOTAL
+    total += parseFloat(producto.precio.replace("S/", ""));
+  });
+
+  // MOSTRAR TOTAL
+  totalCarrito.textContent = `S/ ${total}`;
+}
 
 const checkLibros = document.getElementById("libros");
 const checkPapeleria = document.getElementById("papeleria");
@@ -19,39 +94,35 @@ const checkEscritura = document.getElementById("escritura");
 
 const productos = document.querySelectorAll(".producto-card");
 
-function filtrarProductos(){
+function filtrarProductos() {
+  productos.forEach((producto) => {
+    const categoria = producto.dataset.categoria;
 
-    productos.forEach(producto => {
+    let mostrar = false;
 
-        const categoria = producto.dataset.categoria;
+    if (checkLibros.checked && categoria === "libros") {
+      mostrar = true;
+    }
 
-        let mostrar = false;
+    if (checkPapeleria.checked && categoria === "papeleria") {
+      mostrar = true;
+    }
 
-        if(checkLibros.checked && categoria === "libros"){
-            mostrar = true;
-        }
+    if (checkEscritura.checked && categoria === "escritura") {
+      mostrar = true;
+    }
 
-        if(checkPapeleria.checked && categoria === "papeleria"){
-            mostrar = true;
-        }
+    // Mostrar todos si no hay filtros
+    if (
+      !checkLibros.checked &&
+      !checkPapeleria.checked &&
+      !checkEscritura.checked
+    ) {
+      mostrar = true;
+    }
 
-        if(checkEscritura.checked && categoria === "escritura"){
-            mostrar = true;
-        }
-
-        // Mostrar todos si no hay filtros
-        if(
-            !checkLibros.checked &&
-            !checkPapeleria.checked &&
-            !checkEscritura.checked
-        ){
-            mostrar = true;
-        }
-
-        producto.style.display = mostrar ? "block" : "none";
-
-    });
-
+    producto.style.display = mostrar ? "block" : "none";
+  });
 }
 
 checkLibros.addEventListener("change", filtrarProductos);
